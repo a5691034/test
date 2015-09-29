3357912621ce0b9511f3ba2d28fff6cbf237cec3
Apple <apple@AppledeiMac.local>
2015-09-29 14:55:30 +0800

    Initial Commit

---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Apple on 15/9/29.
+//  Copyright (c) 2015年 williams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by Apple on 15/9/29.
+//  Copyright (c) 2015年 williams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
